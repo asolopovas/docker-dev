ee3b08849bfa0468e1b6fd71a725634150f1ffef
Andrius Solopovas <andrius.solopovas@gmail.com>
2021-01-19 20:42:22 +0000

    save

--- app/livereload.js
+++ app/livereload.js
@@ -1,3 +1,5 @@
 let livereload = require('livereload');
-let server = livereload.createServer();
+let server = livereload.createServer({
+    exts: [html,css,js,png,gif,jpg,php,php5,py,rb,erb,coffee,scss,twig]
+});
 server.watch(__dirname);
