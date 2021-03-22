1bac8b11b1d7b0e7c907bdc81873a0229095414f
Andrius Solopovas <andrius.solopovas@gmail.com>
2021-03-22 14:54:06 +0000

    save

--- app/entry.sh
+++ app/entry.sh
@@ -10,7 +10,7 @@ fi
 
 sudo sed -i -e "s/upload_max_filesize = 2M/upload_max_filesize = 64M/g" $PHP_INI_DIR/php.ini
 sudo sed -i -e "s/post_max_size = 8M/post_max_size = 64M/g" $PHP_INI_DIR/php.ini
-sudo sed -i -e "s/memory_limit = 128M/memory_limit = 256M/g" $PHP_INI_DIR/php.ini
+sudo sed -i -e "s/memory_limit = 128M/memory_limit = 1024M/g" $PHP_INI_DIR/php.ini
 sudo sed -i -e "s/;error_log = syslog/error_log = \/proc\/self\/fd\/2/g" $PHP_INI_DIR/php.ini
 
 if [ ! -f /var/www/project/.env ] && [ -f /var/www/project/.env.example ]; then
